11fd05399b13f7f57126e589eda5626b4f8d21c2
Iliyan Todorov <thracefields@localhost.localdomain>
2020-10-02 21:35:26 +0300

    Added other seeders and factories

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Category;
+use Faker\Generator as Faker;
+
+$factory->define(Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'price' => $faker->numberBetween(2, 20)
+    ];
+});

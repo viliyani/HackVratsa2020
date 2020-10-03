d03913fbca64829831b3d7dd4f0e43fc213922cb
Iliyan Todorov <thracefields@localhost.localdomain>
2020-10-03 11:14:16 +0300

    Style changes made

--- resources/views/layouts/includes/users/menu.blade.php
+++ resources/views/layouts/includes/users/menu.blade.php
@@ -1,11 +1,12 @@
 <div class="card">
-    <h5 class="card-header">Меню</h5>
+    <h5 class="card-header">Навигация</h5>
     <div class="card-body vertical-menu-card">
         <div class="row">
             <div class="col-md-12">
                 <div class="list-group">
-                    <a href="#" class="list-group-item list-group-item-action">
-                    </a>
+                    @if ($user->hasRole('assistant'))
+                    <a href="#" class="list-group-item list-group-item-action">Асистент</a>
+                    @endif
                     <a href="#" class="list-group-item list-group-item-action">
                         Текст
                     </a>

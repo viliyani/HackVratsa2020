55ea1d7a0f105ddfe7ae03393a0cd6cfe218562a
Iliyan Todorov <thracefields@localhost.localdomain>
2020-10-03 11:16:52 +0300

    Style changes made

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -5,32 +5,7 @@
     <div class="container">
         <div class="row mb-5">
             <div class="col-md-3">
-                <div class="card bg-secondary">
-                    <h5 class="card-header text-white">Меню</h5>
-                    <div class="card-body vertical-menu-card">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="list-group">
-                                    <a href="#" class="list-group-item list-group-item-action">
-                                        Текст
-                                    </a>
-                                    <a href="#" class="list-group-item list-group-item-action">
-                                        Текст
-                                    </a>
-                                    <a href="#" class="list-group-item list-group-item-action">
-                                        Текст
-                                    </a>
-                                    <a href="#" class="list-group-item list-group-item-action">
-                                        Текст
-                                    </a>
-                                    <a href="#" class="list-group-item list-group-item-action">
-                                        Текст
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @include('layouts.includes.users.menu')
             </div>
             <div class="col-md-9 mb-5">
                 <div class="card">
